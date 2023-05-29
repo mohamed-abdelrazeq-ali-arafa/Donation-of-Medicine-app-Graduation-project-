fd4c82c726c940f23040f76dee23f37f504f9c2b
mohamed-abdelrazeq-ali-arafa <midoaraf867@gmail.com>
2023-05-29 14:55:01 +0300

    diagnoses result attribute

--- src/main/java/com/medicalsystem/Medical/service/entity/DiagnosesResult.java
+++ src/main/java/com/medicalsystem/Medical/service/entity/DiagnosesResult.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection ="diagnosesresult")
 public class DiagnosesResult {
@@ -11,7 +13,8 @@ public class DiagnosesResult {
     @Id
    private String id;
 
-
+    private String diseaseId;
+    private List<String> medicationId;
     private String DoctorId;
     private status state;
     private Long createdAt;
